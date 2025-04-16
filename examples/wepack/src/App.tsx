8ea2f747fad1b8ad3a0c9d4a5794d5c5b83e5950
fred <lemonpaimc@126.com>
2025-04-17 02:32:25 +0800

    feature: vite 按需加载插件

---
+++ examples/wepack/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@horadrim/components';
+const App: React.FC = () => {
+  return (
+    <div className="app">
+        <Button />
+      <h1>React + TypeScript + Webpack + Babel + Less</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ examples/wepack/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx', // 入口文件
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development', // 开发模式
+  devtool: 'source-map', // 生成 source map
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'], // 支持的文件扩展名
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/, // 处理 TypeScript 文件
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/, // 处理 Less 文件
+        use: [
+          MiniCssExtractPlugin.loader, // 提取 CSS 到单独文件
+          'css-loader',
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/, // 处理图片文件
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(), // 压缩 JS
+      new OptimizeCssAssetsPlugin(), // 压缩 CSS 并去重
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // 清理 dist 文件夹
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // HTML 模板文件
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].css', // 输出的 CSS 文件名
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 3000, // 开发服务器端口
+    open: true, // 自动打开浏览器
+    hot: true, // 热更新
+  },
+};
